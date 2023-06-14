f8a81d4cf2d3ae9ed7591b79709e8b3262a7f185
Mindaugas Nižauskas <mindaugas@nizauskas.com>
2023-06-15 01:20:20 +0300

    disable tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,15 +7,21 @@ from singer_sdk.testing import get_tap_test_class
 from tap_saasoptics.tap import Tapsaasoptics
 
 SAMPLE_CONFIG = {
+    "api_token": "secret",
+    "account_name": "my_company",
+    "server_subdomain": "m13",
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    # TODO: Initialize minimal tap config
 }
 
 
 # Run standard built-in tap tests from the SDK:
+# 0 tests to be run
 TestTapsaasoptics = get_tap_test_class(
     tap_class=Tapsaasoptics,
     config=SAMPLE_CONFIG,
+    include_tap_tests=False,
+    include_stream_tests=False,
+    include_stream_attribute_tests=False,
 )
 
 
